9efe026c5a31bd2e9bc51985db3e7c3136ed74c9
IndraaA07 <tudeindra07@gmail.com>
2024-06-05 23:08:54 +0800

    menambahkan API

---
+++ lib/pages/bookmark_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BookmarkPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Bookmark Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
