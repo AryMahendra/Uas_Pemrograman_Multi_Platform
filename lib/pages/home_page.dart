4d90196e7ec738206355fc6295ef82feaeeede85
IndraaA07 <tudeindra07@gmail.com>
2024-06-05 21:13:28 +0800

    Pembuatan homepage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePageContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 345),
+              decoration: BoxDecoration(
+                border: Border.all(color: Color(0x7DD9D9D9)),
+              ),
+              child: Container(
+                padding: EdgeInsets.fromLTRB(0, 31.5, 0.6, 32.5),
+                child: 
+                Text(
+                  'Berita Terkini',
+                  style: GoogleFonts.getFont(
+                    'Merriweather',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 17,
+                    height: 1.5,
+                    color: Color(0xFF000000),
+                  ),
+                ),
+              ),
+            ),
+          // Berita Se
